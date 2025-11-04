201727f5e229ebbb9db6f3097e9e5fe55266f678
Chamika Jayarathna <failgames.lk@gmail.com>
2025-11-04 21:33:53 +0530

    create data base connection

--- src/bank/management/system/DBConnection.java
+++ src/bank/management/system/DBConnection.java
@@ -1,4 +1,20 @@
 package bank.management.system;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DBConnection {
+
+    Connection connection;
+    Statement statement;
+
+    public DBConnection() {
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","12345678");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
