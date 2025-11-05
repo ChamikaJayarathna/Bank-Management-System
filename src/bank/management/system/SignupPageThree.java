55178cb6a3592fa2bb380eef13635db4920bcfee
Chamika Jayarathna <failgames.lk@gmail.com>
2025-11-05 19:59:37 +0530

    update UI version one on the SignupPageThree.java class

--- src/bank/management/system/SignupPageThree.java
+++ src/bank/management/system/SignupPageThree.java
@@ -1,5 +1,188 @@
 package bank.management.system;
 
-public class SignupPageThree {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class SignupPageThree extends JFrame {
+
+    JRadioButton r1, r2, r3, r4;
+    JCheckBox c1, c2, c3, c4, c5, c6;
+    JButton submit, cancel;
+
+    public SignupPageThree() {
+        super("APPLICATION FORM");
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2 = i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image1 = new JLabel(i3);
+        image1.setBounds(25,10,100,100);
+        add(image1);
+
+        JLabel l1 = new JLabel("Page 3");
+        l1.setFont(new Font("Ralway",Font.BOLD,22));
+        l1.setBounds(400,30,600,30);
+        add(l1);
+
+        JLabel l2 = new JLabel("Account Details");
+        l2.setFont(new Font("Ralway",Font.BOLD,22));
+        l2.setBounds(350,60,600,30);
+        add(l2);
+
+        JLabel l3 = new JLabel("Account Type :");
+        l3.setFont(new Font("Ralway",Font.BOLD,20));
+        l3.setBounds(100,190,160,30);
+        add(l3);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Raleway",Font.BOLD,16));
+        r1.setBackground(new Color(255, 255, 255));
+        r1.setBounds(100,240,160,30);
+        add(r1);
+
+        r2 = new JRadioButton("Fixed Deposit Account");
+        r2.setFont(new Font("Raleway",Font.BOLD,16));
+        r2.setBackground(new Color(255, 255, 255));
+        r2.setBounds(350,240,300,30);
+        add(r2);
+
+        r3 = new JRadioButton("Current Account");
+        r3.setFont(new Font("Raleway",Font.BOLD,16));
+        r3.setBackground(new Color(255, 255, 255));
+        r3.setBounds(100,290,250,30);
+        add(r3);
+
+        r4 = new JRadioButton("Recurring Deposit Account");
+        r4.setFont(new Font("Raleway",Font.BOLD,16));
+        r4.setBackground(new Color(255, 255, 255));
+        r4.setBounds(350,290,260,30);
+        add(r4);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(r1);
+        buttonGroup.add(r2);
+        buttonGroup.add(r3);
+        buttonGroup.add(r4);
+
+        JLabel l4 = new JLabel("Card Number :");
+        l4.setFont(new Font("Ralway",Font.BOLD,20));
+        l4.setBounds(100,340,190,30);
+        add(l4);
+
+        JLabel l5 = new JLabel("(Your 16-digit Card Number)");
+        l5.setFont(new Font("Raleway",Font.BOLD,12));
+        l5.setBounds(100,370,200,20);
+        add(l5);
+
+        JLabel l6 = new JLabel("XXXX-XXXX-XXXX-4841");
+        l6.setFont(new Font("Raleway",Font.BOLD,20));
+        l6.setBounds(360,340,250,30);
+        add(l6);
+
+        JLabel l7 = new JLabel("(It would appear on atm card/cheque Book and Statements)");
+        l7.setFont(new Font("Raleway",Font.BOLD,12));
+        l7.setBounds(360,370,500,20);
+        add(l7);
+
+        JLabel l8 = new JLabel("PIN :");
+        l8.setFont(new Font("Raleway",Font.BOLD,20));
+        l8.setBounds(100,420,200,30);
+        add(l8);
+
+        JLabel l9 = new JLabel("XXXX");
+        l9.setFont(new Font("Raleway",Font.BOLD,20));
+        l9.setBounds(360,420,200,30);
+        add(l9);
+
+        JLabel l10 = new JLabel("(4-digit Password)");
+        l10.setFont(new Font("Raleway",Font.BOLD,12));
+        l10.setBounds(100,450,200,20);
+        add(l10);
+
+        JLabel l11 = new JLabel("Services Required :");
+        l11.setFont(new Font("Raleway",Font.BOLD,20));
+        l11.setBounds(100,500,200,30);
+        add(l11);
+
+        c1 = new JCheckBox("ATM CARD");
+        c1.setBackground(new Color(215,252,252));
+        c1.setFont(new Font("Raleway",Font.BOLD,16));
+        c1.setBounds(100,550,200,30);
+        add(c1);
+
+        c2 = new JCheckBox("Internet Banking");
+        c2.setBackground(new Color(215,252,252));
+        c2.setFont(new Font("Raleway",Font.BOLD,16));
+        c2.setBounds(350,550,200,30);
+        add(c2);
+
+        c3 = new JCheckBox("Mobile Banking");
+        c3.setBackground(new Color(215,252,252));
+        c3.setFont(new Font("Raleway",Font.BOLD,16));
+        c3.setBounds(100,600,200,30);
+        add(c3);
+
+        c4 = new JCheckBox("EMAIL Alerts");
+        c4.setBackground(new Color(215,252,252));
+        c4.setFont(new Font("Raleway",Font.BOLD,16));
+        c4.setBounds(350,600,200,30);
+        add(c4);
+
+        c5 = new JCheckBox("Cheque Book");
+        c5.setBackground(new Color(215,252,252));
+        c5.setFont(new Font("Raleway",Font.BOLD,16));
+        c5.setBounds(100,650,200,30);
+        add(c5);
+
+        c6 = new JCheckBox("E-Statement");
+        c6.setBackground(new Color(215,252,252));
+        c6.setFont(new Font("Raleway",Font.BOLD,16));
+        c6.setBounds(350,650,200,30);
+        add(c6);
+
+        JCheckBox c7 = new JCheckBox("I here by decleares that the above entered details correct to the best of my knlowledge.",true);
+        c7.setBackground(new Color(215,252,252));
+        c7.setFont(new Font("Raleway",Font.BOLD,12));
+        c7.setBounds(100,730,600,20);
+        add(c7);
+
+        JLabel l12 = new JLabel("Form No :");
+        l12.setFont(new Font("Ralway",Font.BOLD,20));
+        l12.setBounds(650,30,100,30);
+        add(l12);
+
+        JLabel l13 = new JLabel();
+        l13.setFont(new Font("Ralway",Font.BOLD,20));
+        l13.setBounds(700,30,100,30);
+        add(l13);
+
+        submit = new JButton("Submit");
+        submit.setFont(new Font("Ralewy",Font.BOLD,14));
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        submit.setOpaque(true);
+        submit.setBorderPainted(false);
+        submit.setBounds(250,780,100,30);
+        add(submit);
+
+        cancel = new JButton("Cancel");
+        cancel.setFont(new Font("Ralewy",Font.BOLD,14));
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        cancel.setOpaque(true);
+        cancel.setBorderPainted(false);
+        cancel.setBounds(420,780,100,30);
+        add(cancel);
+
+        getContentPane().setBackground(new Color(222,255,228));
+        setLayout(null);
+        setSize(850,880);
+        setLocation(360,40);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new SignupPageThree();
+    }
 }
